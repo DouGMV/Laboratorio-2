d961dbbd95043ae58f8c423e9f35808c0e6940e2
Sir_d <Sir_d@DOUVICTUS>
2024-02-08 09:01:26 -0600

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laboratorio__2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Resize += new System.EventHandler(this.Form_Resize);
+        }
+
+        private void Form_Resize(object sender, EventArgs e)
+        {
+            webView.Size = this.ClientSize - new System.Drawing.Size(webView.Location);
+            goButton.Left = this.ClientSize.Width - goButton.Width;
+            comboBox1.Width = goButton.Left - comboBox1.Left;
+        }
+
+        private void BotonIr_Click(object sender, EventArgs e)
+        {
+            if (webView != null && webView.CoreWebView2 != null)
+            {
+                webView.CoreWebView2.Navigate(comboBox1.Text);
+            }
+        }
+
+        private void inicioToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoHome();
+        }
+
+        private void haciaAtrasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void haciaDelanteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+            webBrowser1.GoHome();
+        }
+
+        private void webView_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
